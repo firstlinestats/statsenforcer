687aeda9361225b449d459d69fd820829b40fb5d
Spencer Van Cise <slvancise@gmail.com>
2016-10-08 21:09:25 +0000

    added models

--- statsenforcer/website/views.py
+++ statsenforcer/website/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-
+from team.models import Team
 
 def index(request):
+    teams = Team.objects.all()
+    print teams
     return HttpResponse("Hello, world. You're at the main page.")
