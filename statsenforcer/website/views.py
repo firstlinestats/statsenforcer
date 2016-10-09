040486e92b2f2523c5af7c329f59c2daee68c941
Matt Daye <mdaye13@gmail.com>
2016-10-09 19:59:40 -0400

    changed index to new season and show today's games

--- statsenforcer/website/views.py
+++ statsenforcer/website/views.py
@@ -7,7 +7,7 @@ import json
 # Create your views here.
 
 def index(request):
-    games = Game.objects.filter(season=20152016).order_by('-dateTime', '-gamePk')[:30]
+    games = Game.objects.filter(season=20162017, dateTime__lte=datetime.date.today()).order_by('-dateTime', '-gamePk')[:30]
 
     #teams = Team.objects.all()
     teamdata = {}
