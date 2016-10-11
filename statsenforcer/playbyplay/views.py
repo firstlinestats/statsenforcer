4801d74e85d3ae05d16a308e17aa1694af5995b3
Matt Daye <mdaye13@gmail.com>
2016-10-11 15:21:40 -0400

    css and game changes

--- statsenforcer/playbyplay/views.py
+++ statsenforcer/playbyplay/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 
+import fancystats
+
 from . import models
+from team import models as tmodels
 
 # Create your views here.
 def game(request, game_pk):
@@ -10,4 +13,12 @@ def game(request, game_pk):
         context["period"] = models.GamePeriod.objects.filter(game_id=game_pk).latest("startTime")
     except:
         context["period"] = None
+    context["playbyplay"] = models.PlayByPlay.objects.filter(gamePk_id=game_pk).order_by("eventIdx")
+    context["periodTime"] = str(context["playbyplay"].last().periodTime)[:-3]
+    pbpdict = [x.__dict__ for x in context["playbyplay"]]
+    context["teamstats"] = fancystats.team.get_stats(pbpdict)
+    for ts in context["teamstats"]:
+        team = get_object_or_404(tmodels.Team, id=ts)
+        context["teamstats"][ts]["team"] = team.abbreviation
+    context["teamstats"] = context["teamstats"].values()
     return render(request, 'games/game.html', context)
