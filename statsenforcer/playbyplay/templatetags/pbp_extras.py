9d7d89ea6fdd2492ebedcab2dd69b5040b2bd1c4
Matt Daye <mdaye13@gmail.com>
2016-10-14 17:15:32 -0400

    growing changes for pbp

--- statsenforcer/playbyplay/templatetags/pbp_extras.py
+++ statsenforcer/playbyplay/templatetags/pbp_extras.py
@@ -1,15 +1,106 @@
 from django import template
 
+from fancystats import constants
+
 register = template.Library()
 
 @register.filter
 def get_period(period):
-    if period.period == 1:
+    if period == 1:
         periodTime = "1st Period"
     elif period == 2:
         periodTime = "2nd Period"
     elif period == 3:
         periodTime = "3rd Period"
     else:
-        periodTime = "Overtime (" + str(period.period) + ")"
+        periodTime = "Overtime (" + str(period) + ")"
     return periodTime
+
+
+@register.filter
+def create_play_text(play):
+    if play["playType"] == "GAME_SCHEDULED":
+        return "Game has been scheduled"
+    elif play["playType"] == "PERIOD_READY":
+        return get_period(play["period"]) + " is ready"
+    elif play["playType"] == "PERIOD_START":
+        return get_period(play["period"]) + " has started"
+    elif play["playType"] == "FACEOFF":
+        winner = ""
+        loser = ""
+        for player in play["players"]:
+            if player["player_type"] == 1:
+                winner = player["player__fullName"]
+            else:
+                loser = player["player__fullName"]
+        return winner + " won faceoff against " + loser
+    elif play["playType"] == "HIT":
+        hitter = ""
+        hittee = ""
+        for player in play["players"]:
+            if player["player_type"] == 3:
+                hitter = player["player__fullName"]
+            else:
+                hittee = player["player__fullName"]
+        return hitter + " hit " + hittee
+    elif play["playType"] == "SHOT":
+        shooter = ""
+        goalie = ""
+        for player in play["players"]:
+            if player["player_type"] == 7:
+                shooter = player["player__fullName"]
+            else:
+                goalie = player["player__fullName"]
+        return shooter + " shot, saved by " + goalie
+    elif play["playType"] == "BLOCKED_SHOT":
+        shooter = ""
+        blocker = ""
+        for player in play["players"]:
+            if player["player_type"] == 7:
+                shooter = player["player__fullName"]
+            else:
+                blocker = player["player__fullName"]
+        return shooter + " shot, blocked by " + blocker
+    elif play["playType"] == "MISSED_SHOT":
+        shooter = ""
+        for player in play["players"]:
+            shooter = player["player__fullName"]
+        return shooter + " shot, missed"
+    elif play["playType"] == "GIVEAWAY":
+        playername = ""
+        for player in play["players"]:
+            playername = player["player__fullName"]
+        return playername + " giveaway"
+    elif play["playType"] == "TAKEAWAY":
+        playername = ""
+        for player in play["players"]:
+            playername = player["player__fullName"]
+        return playername + " takeaway"
+    elif play["playType"] == "STOP":
+        return "Play stopped"
+    elif play["playType"] == "PERIOD_END":
+        return "Period has ended"
+    elif play["playType"] == "PERIOD_OFFICIAL":
+        return "Period has been marked as official"
+    elif play["playType"] == "GOAL":
+        print play
+        shooter = ""
+        assist1 = ""
+        assist2 = ""
+        for player in play["players"]:
+            if player["player_type"] == 5:
+                shooter = player["player__fullName"]
+            elif player["player_type"] == 6:
+                assist1 = player["player__fullName"]
+            elif player["player_type"] == 16:
+                assist2 = player["player__fullName"]
+        info = shooter + " scored (" + play["shotType"] + ")"
+        if assist1 != "":
+            info += ", assisted by " + assist1
+        if assist2 != "":
+            info += ", and " + assist2
+        return "<b>" + info + "</b>"
+    else:
+        print play["playType"]
+
+    return ""
--- statsenforcer/playbyplay/views.py
+++ statsenforcer/playbyplay/views.py
@@ -15,11 +15,39 @@ def game(request, game_pk):
         context["period"] = None
     if context["period"] is not None:
         context["playbyplay"] = models.PlayByPlay.objects.filter(gamePk_id=game_pk).order_by("eventIdx")
-        context["periodTime"] = str(context["playbyplay"].last().periodTime)[:-3]
-        pbpdict = [x.__dict__ for x in context["playbyplay"]]
-        context["teamstats"] = fancystats.team.get_stats(pbpdict, context["game"].homeTeam.id, context["game"].awayTeam.id)
+        context["playbyplay"] = [x.__dict__ for x in context["playbyplay"]]
+
+        poi_data = models.PlayerOnIce.objects.values("player_id", "play_id", "play__homeScore", "play__awayScore").filter(play_id__in=[x["id"] for x in context["playbyplay"]])
+
+        playerteams = models.PlayerGameStats.objects.values("team__abbreviation", "player_id").filter(game_id=game_pk)
+        p2t = {}
+        for p in playerteams:
+            p2t[p["player_id"]] = p["team__abbreviation"]
+        goalieteams = models.GoalieGameStats.objects.values("team__abbreviation", "player_id").filter(game_id=game_pk)
+        for p in goalieteams:
+            p2t[p["player_id"]] = p["team__abbreviation"]
+
+        pip_data = models.PlayerInPlay.objects.values("player_type", "play_id", "player__fullName", "player_id").filter(play_id__in=[x["id"] for x in context["playbyplay"]])
+        pipdict = {}
+        for poi in pip_data:
+            if poi["play_id"] not in pipdict:
+                pipdict[poi["play_id"]] = []
+            if poi["player_id"] in p2t:
+                poi["player__fullName"] += " (" + p2t[poi["player_id"]] + ")"
+            pipdict[poi["play_id"]].append(poi)
+
+        context["periodTimeString"] = str(context["playbyplay"][-1]["periodTime"])[:-3]
+        for play in context["playbyplay"]:
+            play["periodTimeString"] = str(play["periodTime"])[:-3]
+            if play["id"] in pipdict:
+                play["players"] = pipdict[play["id"]]
+            else:
+                play["players"] = []
+        
+        context["teamstats"] = fancystats.team.get_stats(context["playbyplay"], context["game"].homeTeam.id, context["game"].awayTeam.id)
         for ts in context["teamstats"]:
             team = get_object_or_404(tmodels.Team, id=ts)
             context["teamstats"][ts]["team"] = team.abbreviation
+
         context["teamstats"] = context["teamstats"].values()
     return render(request, 'games/game.html', context)
