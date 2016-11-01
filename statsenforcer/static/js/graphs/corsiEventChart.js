aa7812be2b3068183add32b7f268adf3b9879181
Spencer Van Cise <slvancise@gmail.com>
2016-11-01 18:31:34 -0400

    added corsi event chart

---
+++ statsenforcer/static/js/graphs/corsiEventChart.js
@@ -0,0 +1,224 @@
+function create_corsi_events(alldata, divid, teamname) {
+  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = (900 - margin.left) / 2 - margin.left - margin.right,
+    height = width;
+
+  $(divid).width(($("#gameTabContent").width()) / 2).height(($("#gameTabContent").width()) / 2);
+  if ($(divid).width() < 768 / 2) {
+    width = (900 - margin.left) - margin.left - margin.right;
+    height = width;
+    $(divid).width($("#gameTabContent").width()).height($("#gameTabContent").width());
+  }
+  var data = [];
+  var existing = {};
+  for (key in alldata) {
+    var cf = alldata[key]["cf"];
+    var ca = alldata[key]["ca"];
+    var name = alldata[key].name;
+    var sf = alldata[key].sf;
+    var sa = alldata[key].sa;
+    var toi = alldata[key].toi;
+    var dupe = false;
+    var newindex = data.length;
+    if (cf in existing) {
+      if (ca in existing[cf]) {
+        dupe = true;
+      } else {
+        existing[cf][ca] = newindex;
+      }
+    } else {
+      existing[cf] = {};
+      existing[cf][ca] = newindex;
+    }
+    if (dupe == true) {
+      var prev = data[existing[cf][ca]];
+      prev.sf += ", " + alldata[key]["sf"];
+      prev.sa += ", " + alldata[key]["sa"];
+      prev.toi += ", " + alldata[key]["toi"];
+      prev.name += ", " + alldata[key]["name"];
+    } else {
+      var prev = {};
+      prev.cf = alldata[key]["cf"];
+      prev.ca = alldata[key]["ca"];
+      prev.sf = alldata[key]["sf"];
+      prev.sa = alldata[key]["sa"];
+      prev.toi = alldata[key]["toi"];
+      prev.name = alldata[key]["name"];
+      data.push(prev);
+    }
+  }
+
+  /* 
+   * value accessor - returns the value to encode for a given data object.
+   * scale - maps value to a visual display encoding, such as a pixel position.
+   * map function - maps from data value to display value
+   * axis - sets up axis
+   */ 
+  // setup x 
+  var xValue = function(d) { return d.cf + d.ca;}, // data -> value
+      xScale = d3.scale.linear().range([0, width]), // value -> display
+      xMap = function(d) { return xScale(xValue(d));}, // data -> display
+      xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+  // setup y
+  var yValue = function(d) { return d.cf - d.ca;}, // data -> value
+      yScale = d3.scale.linear().range([height, 0]), // value -> display
+      yMap = function(d) { return yScale(yValue(d));}, // data -> display
+      yAxis = d3.svg.axis().scale(yScale).orient("left");
+  // setup fill color
+  var cValue = function(d) { return "black";},
+      color = d3.scale.category10();
+  // add the tooltip area to the webpage
+  for (var i=0; i<data.length; i++) {
+    d3.select("body")
+        .append("div")
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+        .style("border-radius", 3)
+        .attr("active", false)
+        .attr("class", "tooltip-inner")
+        .attr("id", teamname + "-" + data[i].cf + "-" + data[i].ca + "-tooltip")
+        .text("a simple tooltip");
+  }
+
+  // add the graph canvas to the body of the webpage
+  var svg = d3.select(divid).append("svg")
+      .attr("width", $(divid).width())
+      .attr("height", $(divid).width())
+      .attr("viewBox", "0 0 " + (width + margin.left + margin.right) + " " + (height + margin.top + margin.bottom))
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  // don't want dots overlapping axis, so add in buffer to data domain
+  xScale.domain([d3.min([0, d3.min(data, xValue)-1]), d3.max(data, xValue)+1]);
+  yScale.domain([-d3.max(data, function(d) { return Math.abs(d.cf - d.ca) + 1; }), d3.max(data,  function(d) { return Math.abs(d.cf - d.ca); }) + 1]);
+  svg.append("rect")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .attr("transform", "translate(-" + margin.left + ",-" + margin.top + ")")
+      .attr("fill", "white");
+  // x-axis
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("x", width)
+      .attr("y", -6)
+      .style("text-anchor", "end")
+      .text("Corsi For Plus Corsi Against");
+  // y-axis
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Corsi For Minus Corsi Against");
+  function format_toi(t) {
+    var minutes = Math.floor(t / 60);
+    var seconds = t - minutes * 60;
+    return minutes + ":" + seconds
+  }
+    svg.append("text")
+        .attr("x", margin.left + 50)
+        .attr("y", height - margin.bottom)
+        .attr("text-anchor", "middle")
+        .style("font-size", "20px")
+        .style("fill", "grey")
+        .text("firstlinestats.com")
+    svg.append("text")
+        .attr("x", (width / 2))             
+        .attr("y", 0 - (margin.top / 4))
+        .attr("text-anchor", "middle")  
+        .style("font-size", "16px") 
+        .style("text-decoration", "underline")  
+        .text(teamname + " On-Ice Shot Attempts For/Against");
+  // draw dots
+  svg.selectAll(".dot")
+      .data(data)
+    .enter().append("circle")
+      .attr("class", "dot")
+      .attr("r", 5)
+      .attr("cx", xMap)
+      .attr("cy", yMap)
+      .style("fill", function(d) { return get_color(teamname, true); })
+      .style("stroke", function(d) { return get_color(teamname, false); })
+      .on("click", mouseover)
+
+    function mouseover(p) {
+        var tooltip = d3.select("#" + teamname + "-" + p.cf + "-" + p.ca + "-tooltip");
+        var active = tooltip.attr("active"),
+          newOpacity = active ? 0 : 1;
+        tooltip.html(p["name"] + "<br />SF:" + p.cf + "<br />SA:" + p.ca + "<br />TOI:" + p.toi);
+        tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");
+        if (active == "false") {
+          tooltip.style("visibility", "visible")
+          tooltip.attr("active", "true")
+          if ($("#" + teamname + "-" + p.cf + "-" + p.ca + "-name").length == 0) {
+            svg.append("text")
+              .attr("id", teamname + "-" + p.cf + "-" + p.ca + "-name")
+              .attr("x", xMap(p))
+              .attr("y", yMap(p) - 5)
+              .attr("text-anchor", "middle")  
+              .style("font-size", "16px")
+              .text(p.name);
+          }
+        } else {
+          tooltip.style("visibility", "hidden")
+          tooltip.attr("active", "false")
+        }
+    }
+    function mouseoverName(p) {
+        var tooltip = d3.select("#" + teamname + "-" + p.cf + "-" + p.ca + "-tooltip");
+        var active = tooltip.attr("active"),
+          newOpacity = active ? 0 : 1;
+        tooltip.html(p["name"] + "<br />SF:" + p.cf + "<br />SA:" + p.ca + "<br />TOI:" + p.toi);
+        tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");
+        if (active == "false") {
+          tooltip.style("visibility", "visible")
+          tooltip.attr("active", "true")
+          svg.append("text")
+            .attr("id", "#" + teamname + "-" + p.cf + "-" + p.ca + "-name")
+            .attr("x", xMap(p))
+            .attr("y", yMap(p) - 5)
+            .attr("text-anchor", "middle")  
+            .style("font-size", "16px")
+            .on("click", mouseoverName)
+            .text(p.name);
+        } else {
+          $("#" + teamname + "-" + p.cf + "-" + p.ca + "-name").remove();
+          tooltip.style("visibility", "hidden")
+          tooltip.attr("active", "false")
+        }
+    }
+    function mouseout() {
+        d3.selectAll("text").classed("active", false);
+        tooltip.style("visibility", "hidden");
+    }
+    // Draw Lines
+    svg.append("line")          // attach a line
+        .style("stroke", "red")  // colour the line
+        .attr("x1", xScale(d3.min([0, d3.min(data, xValue)-1])))     // x position of the first end of the line
+        .attr("y1", yScale(0))      // y position of the first end of the line
+        .attr("x2", xScale(d3.max(data, xValue)+1))     // x position of the second end of the line
+        .attr("y2", yScale(0));    // y position of the second end of the line
+    var guideLines = [0.5, 1, 2, 4];
+    for (var i = 0; i<guideLines.length; i++) {
+        svg.append("line")          // attach a line
+            .style("stroke", "grey")  // colour the line
+            .attr("x1", xScale(d3.min([0, d3.min(data, xValue)-1])))     // x position of the first end of the line
+            .attr("y1", yScale(0))      // y position of the first end of the line
+            .attr("x2", xScale(guideLines[i] * d3.max(data, function(d) { return Math.abs(d.cf - d.ca) + 1; })))     // x position of the second end of the line
+            .attr("y2", yScale(-d3.max(data, function(d) { return Math.abs(d.cf - d.ca) + 1; })));    // y position of the second end of the line
+        svg.append("line")          // attach a line
+            .style("stroke", "grey")  // colour the line
+            .attr("x1", xScale(d3.min([0, d3.min(data, xValue)-1])))     // x position of the first end of the line
+            .attr("y1", yScale(0))      // y position of the first end of the line
+            .attr("x2", xScale(guideLines[i] * d3.max(data, function(d) { return Math.abs(d.cf - d.ca) + 1; })))     // x position of the second end of the line
+            .attr("y2", yScale(d3.max(data, function(d) { return Math.abs(d.cf - d.ca) + 1; })));    // y position of the second end of the line
+    }
+}
---
+++ statsenforcer/static/js/graphs/shotChart.js
@@ -0,0 +1,201 @@
+function shotChart(data, homeabbr, awayabbr) {
+    var margin = {top: 0, right: 0, bottom: 0, left: 0},
+        width = 900 - margin.left - margin.right,
+        height = width * 0.425 + margin.left;
+
+    $("#shotChart").width($("#gameTabContent").width()).height(Math.round($("#gameTabContent").width() * 0.425));
+    var x = d3.scale.linear()
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    x.domain([-100, 100]);
+    y.domain([-42.5, 42.5]);
+
+    var color = d3.scale.category10();
+
+    var line = d3.svg.line()
+        .interpolate("basis")
+        .x(function(d) { return x(d.x); })
+        .y(function(d) { return y(d.y); });
+
+    // add the tooltip area to the webpage
+    var tooltip = d3.select("body")
+        .append("div")
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+        .style("border-radius", 3)
+        .attr("class", "tooltip-inner")
+        .text("a simple tooltip");
+
+    var svg = d3.select("#shotChart").append("svg")
+        .attr("width", $("#shotChart").width())
+        .attr("height", $("#shotChart").height())
+        .attr("viewBox", "0 0 " + width + " " + height)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    svg.append("rect")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .attr("transform", "translate(-" + margin.left + ",-" + margin.top + ")")
+        .attr("fill", "white");
+
+    svg.append("svg:image")
+        .attr("xlink:href", "/static/svg/rink.png")
+        .attr("width", width - margin.left)
+        .attr("height", height - margin.top);
+
+    // Add team logos
+    svg.append("svg:image")
+        .attr("xlink:href", "/static/images/team/" + homeabbr + ".png")
+        .attr("width", (width / 3) - margin.left)
+        .attr("height", (height / 3) - margin.top)
+        .attr("opacity", 0.5)
+        .attr("transform", "translate(" + (width - margin.left - width / 2.75) + "," + ((height - margin.top) / 3) + ")");
+    svg.append("svg:image")
+        .attr("xlink:href", "/static/images/team/" + awayabbr + ".png")
+        .attr("width", (width / 3) - margin.left)
+        .attr("height", (height / 3) - margin.top)
+        .attr("opacity", 0.5)
+        .attr("transform", "translate(" + (width / 20) + "," + ((height - margin.top) / 3) + ")");
+
+    var dangerZoneHome = [{x: 89, y: -9},
+        {x: 69, y: -22}, {x: 54, y: -22},
+        {x: 54, y: -9}, {x: 44, y: -9},
+        {x: 44, y: 9}, {x: 54, y: 9},
+        {x: 54, y: 22}, {x: 69, y: 22},
+        {x: 89, y: 9}, {x: 89, y: -9}];
+
+    var highDangerZoneHome = [{x: 89, y: -9},
+        {x: 69, y: -9}, {x: 69, y: 9},
+        {x: 89, y: 9}, {x: 89, y: -9}];
+
+    var dangerZone = [dangerZoneHome];
+    dangerZone.push(highDangerZoneHome);
+    var dangerZoneAway = [];
+    for (var points in dangerZoneHome) {
+        var p = dangerZoneHome[points];
+        dangerZoneAway.push({x: -p.x, y: p.y});
+    }
+    var highDangerZoneAway = [];
+    for (var points in highDangerZoneHome) {
+        var p = highDangerZoneHome[points];
+        highDangerZoneAway.push({x: -p.x, y: p.y});
+    }
+    dangerZone.push(dangerZoneAway);
+    dangerZone.push(highDangerZoneAway);
+
+
+    svg.selectAll("polygon")
+        .data(dangerZone)
+      .enter().append("polygon")
+        .attr("points",function(d) {
+            return d.map(function(d) {
+                return [x(d.x), y(d.y)].join(",");
+            }).join(" ");
+        })
+        .attr("stroke","black")
+        .attr("fill", "none")
+        .attr("stroke-width", 2)
+        .attr("opacity", 0.1);
+
+    svg.selectAll(".dot")
+        .data(data["home"].concat(data["away"]))
+      .enter().append("circle")
+        .attr("class", "dot")
+        .attr("r", function(d) {
+            if (d.scoring_chance == 1)
+                return 6;
+            else if (d.scoring_chance == 2)
+                return 8;
+            else
+                return 4;
+        })
+        .attr("cx", function(d) { return x(d.x); })
+        .attr("cy", function(d) { return y(d.y); })
+        .style("fill", function(d) {
+            if (d.type == "GOAL") {
+                return "#FF0000";
+            } else if (d.type == "BLOCKED_SHOT") {
+                return "#00FF00";
+            } else if (d.type == "MISSED_SHOT") {
+                return "#000000";
+            } else {
+                return "#0000FF";
+            }
+        })
+        .style("stroke", function(d) {
+            if (d.xcoord > 0)
+                return get_color(homeabbr, true);
+            else
+                return get_color(awayabbr, true);
+        })
+        .style("stroke-width", "2px")
+        .on("mouseover", function(d) {
+            var shotType = "Unknown";
+            if (d.type == "MISSED_SHOT")
+                shotType = "Missed Shot";
+            else if (d.type == "BLOCKED_SHOT")
+                shotType = "Blocked Shot";
+            else if (d.type == "GOAL")
+                shotType = "Goal";
+            else if (d.type == "SHOT")
+                shotType = "Shot";
+            var html = d.description + "<br /><b>Shot Type:</b>" + shotType + "<br /><b>Period: </b>" + d.period + " | " + d.time + "<br />" + d.danger;
+            tooltip.html(html);
+            tooltip.style("visibility", "visible");
+        })
+        .on("mousemove", function(d){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+        .on("mouseout", function(d) {
+            d3.selectAll("text").classed("active", false);
+            tooltip.style("visibility", "hidden");
+        });
+
+    createLegend();
+
+    function createLegend() {
+        var legendRectSize = 18;
+        var legendSpacing = 4;
+        
+        var data = [{name: "Shot on Goal", color: "#0000FF"},
+            {"name": "Missed Shot", color: "#000000"},
+            {"name": "Blocked Shot", color: "#00FF00"},
+            {"name": "Goal", "color": "#FF0000"}]
+        svg.append('rect')
+            .attr('x', width / 2 - (legendRectSize * 4))
+            .attr('y', margin.top + legendRectSize)
+            .attr('width', legendRectSize * 8)
+            .attr('height', legendRectSize * 6)
+            .style('fill', 'white')
+            .style('stroke', 'black')
+        var legend = svg.selectAll('.legend')
+            .data(data)
+          .enter()
+            .append('g')
+            .attr('class', 'legend')
+            .attr('transform', function(d, i) {
+                var height = legendRectSize + legendSpacing;
+                var offset =  height;
+                var horz = -2 * legendRectSize;
+                var vert = i * height + offset;
+                return 'translate(' + horz + ',' + vert + ')';
+            });
+        legend.append('circle')
+            .attr("class", "dot")
+            .attr('cx', width / 2 - legendRectSize * 1)
+            .attr('cy', legendRectSize - 3)
+            .attr('r', legendRectSize / 2)
+            .style('fill', function(d) { return d.color; })
+            .style('stroke', color)
+            .style('opacity', function(d) { return d.opacity; })
+            .style("stroke", function(d) { return "black";});
+        legend.append('text')
+            .attr('x', width / 2)
+            .attr('y', legendRectSize + 5)
+            .style("text-anchor", "start")
+            .text(function(d) { return d.name; });
+    }
+
+}
