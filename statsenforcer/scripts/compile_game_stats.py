cb71369cdc3277a5fccd06967eacdf3d46c95fc9
Matt Daye <mdaye13@gmail.com>
2016-11-24 13:53:51 -0500

    adding gitignore and starting compile game stats

--- statsenforcer/scripts/compile_game_stats.py
+++ statsenforcer/scripts/compile_game_stats.py
@@ -1,4 +1,18 @@
+import os
+import sys
+import django
 
+import datetime
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "statsenforcer"))
+sys.path.append("../")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "statsenforcer.settings")
+from django.conf import settings
+
+django.setup()
+
+from playbyplay.models import Game
+from team.models import TeamGameStats
 
 
 def compile_info(gameId):
@@ -6,7 +20,10 @@ def compile_info(gameId):
 
 
 def main():
-	pass
+	games = TeamGameStats.objects.values_list("game_id", flat=True).all()
+	mgames = Game.objects.values_list("gamePk", flat=True).exclude(gamePk__in=games)\
+		.filter(gameState__in=["5", "6", "7"], season=20162017)
+	print mgames
 
 
 if __name__ == "__main__":
