575ceef59df2ac17b9ba9c180dff6b94cc03a179
Matt Daye <mdaye13@gmail.com>
2016-11-24 13:59:26 -0500

    implementing custom query pull

--- statsenforcer/scripts/compile_game_stats.py
+++ statsenforcer/scripts/compile_game_stats.py
@@ -11,8 +11,10 @@ from django.conf import settings
 
 django.setup()
 
-from playbyplay.models import Game
+from playbyplay.models import Game, PlayByPlay
 from team.models import TeamGameStats
+
+import queries
 
 
 def compile_info(gameId):
@@ -23,7 +25,10 @@ def main():
 	games = TeamGameStats.objects.values_list("game_id", flat=True).all()
 	mgames = Game.objects.values_list("gamePk", flat=True).exclude(gamePk__in=games)\
 		.filter(gameState__in=["5", "6", "7"], season=20162017)
-	print mgames
+	for game in mgames:
+		pbp = list(PlayByPlay.objects.raw(queries.playbyplayquery, [game, ]))
+		print len(pbp)
+		break
 
 
 if __name__ == "__main__":
