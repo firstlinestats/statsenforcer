055d26dd9330c8474da34273159d9144b9fffeae
Matt Daye <mdaye13@gmail.com>
2017-01-22 20:47:10 -0500

    removing htmlmin

--- statsenforcer/statsenforcer/settings.py
+++ statsenforcer/statsenforcer/settings.py
@@ -62,11 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'htmlmin.middleware.HtmlMinifyMiddleware',
-    'htmlmin.middleware.MarkRequestMiddleware',
 ]
-
-HTML_MINIFY = False
 
 ROOT_URLCONF = 'statsenforcer.urls'
 
